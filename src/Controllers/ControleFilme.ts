5755b1e48e00bc03b7563c1fd7fdc07065a84406
Leonardo Pernomian <leonardopernomian@hotmail.com>
2024-08-01 11:00:43 -0300

    fazendo as funções de CRUD, só falta a de editar rs

--- src/Controllers/ControleFilme.ts
+++ src/Controllers/ControleFilme.ts
@@ -4,6 +4,11 @@ import { FilmeModel } from "../Models/Filme";
 
 import Logger from "../../config/logger";
 
+
+
+
+// ----------------------------- CRIAÇÃO --------------------------
+
 export async function criarFilme(requisicao: Request, resposta: Response) {
    try {
       const data = requisicao.body
@@ -11,6 +16,63 @@ export async function criarFilme(requisicao: Request, resposta: Response) {
       return resposta.status(201).json(filme)
    } catch (error: any) {
       Logger.error(`Erro no sistema: ${error.message}`)
+      resposta.status(500).json({error: "tente novamente mais tarde!"})
    }
     
+}
+
+// -------------------------------- FILTRAR POR ID ------------------------------
+
+export async function AcharFilmePorId(requisicao: Request, resposta: Response) {
+   try {
+      const id = requisicao.params.id
+      const filme = await FilmeModel.findById(id)
+
+      if(!filme) {
+         return resposta.status(404).json("filme nao existe")
+      }
+      return resposta.status(200).json(filme)
+   } catch (error: any) {
+      Logger.error(`Erro no sistema: ${error.message}`)
+      resposta.status(500).json({error: "tente novamente mais tarde!"})
+   }
+   
+}
+
+// ---------------------------- ACHAR TODOS OS FILMES ----------------------------------
+
+export async function AcharTodoOsFilmes(requisicao: Request, resposta: Response) {
+   try {
+
+      const filmes = await FilmeModel.find()
+      return resposta.status(200).json(filmes)
+      
+   } catch (error: any) {
+      Logger.error(`Erro no sistema: ${error.message}`)
+      resposta.status(500).json({error: "tente novamente mais tarde!"})
+   }
+   
+}
+
+// ----------------------------------- REMOVER FILME ------------------------------
+
+export async function RemoverFilme(requisicao: Request, resposta: Response) {
+
+   try {
+      const id = requisicao.params.id
+      const filme = await FilmeModel.findById(id)
+
+      if(!filme) {
+         return resposta.status(404).json({error: "este filme nao existe"})
+      }
+      await filme.deleteOne()
+
+      return resposta.status(200).json({msg: "filme removido com sucesso"})
+
+      
+   } catch (error: any) {
+      Logger.error(`Erro no sistema: ${error.message}`)
+      resposta.status(500).json({error: "tente novamente mais tarde!"})
+   }
+   
 }
--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,16 @@
 import { Router, Request, Response } from "express";
-import { criarFilme } from "./Controllers/ControleFilme";
+import { AcharFilmePorId, AcharTodoOsFilmes, criarFilme, RemoverFilme } from "./Controllers/ControleFilme";
 
 // validate
 import { validacao } from "./middleware/Controlar";
 import { ValidacaoDoFilme } from "./middleware/ValidacaodeFilme";
 
+
 const router = Router()
 
 export default router.get("/teste", (requisicao: Request, resposta: Response) => {
     resposta.status(200).send("rota tudo certo")
-}).post("/filme",ValidacaoDoFilme(), validacao, criarFilme) 
+}).post("/filme",ValidacaoDoFilme(), validacao, criarFilme) // CRIAÇÃO DO FILME
+.get("/filme/:id", AcharFilmePorId) // FILTRAR POR ID
+.get("/filme", AcharTodoOsFilmes)  // MOSTRAR TODOS OS FILMES
+.delete("/filme/:id", RemoverFilme) // REMOVER FILME
